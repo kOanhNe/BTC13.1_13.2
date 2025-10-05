79f515573eb7148bbdab6cda88e0fb61032446c6
kOanhNe <k.oanhlh@gmail.com>
2025-10-05 20:53:09 +0700

    Update UserServlet.java

--- src/main/java/controller/UserServlet.java
+++ src/main/java/controller/UserServlet.java
@@ -18,52 +18,44 @@ public class UserServlet extends HttpServlet {
             throws ServletException, IOException {
 
         HttpSession session = request.getSession();
+        String url = "/index.jsp"; // Trang mặc định
 
-        String url = "/index.jsp";
-
-        // get current action
         String action = request.getParameter("action");
         if (action == null) {
-            action = "display_users";  // default action
+            action = "display_users";  // Action mặc định
         }
 
-        // perform action and set URL to appropriate page
         if (action.equals("display_users")) {
-            // get list of users
             List<User> users = UserDB.selectUsers();
             request.setAttribute("users", users);
-        }
+        } 
         else if (action.equals("display_user")) {
             String emailAddress = request.getParameter("email");
             User user = UserDB.selectUser(emailAddress);
             session.setAttribute("user", user);
             url = "/user.jsp";
-        }
+        } 
         else if (action.equals("update_user")) {
-            // get parameters from the request
             String firstName = request.getParameter("firstName");
             String lastName = request.getParameter("lastName");
 
-            // get and update user
             User user = (User) session.getAttribute("user");
-            // user.setEmail(email); // As per note, email should not be updated
             user.setFirstName(firstName);
             user.setLastName(lastName);
             UserDB.update(user);
 
-            // get and set updated users
             List<User> users = UserDB.selectUsers();
             request.setAttribute("users", users);
-        }
+        } 
         else if (action.equals("delete_user")) {
-            // get the user
             String email = request.getParameter("email");
             User user = UserDB.selectUser(email);
 
-            // delete the user
-            UserDB.delete(user);
+            // SỬA LỖI: Luôn kiểm tra user != null trước khi xóa
+            if (user != null) {
+                UserDB.delete(user);
+            }
 
-            // get and set updated users
             List<User> users = UserDB.selectUsers();
             request.setAttribute("users", users);
         }
